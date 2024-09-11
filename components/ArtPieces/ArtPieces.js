a546fef4ee6efce28117deb2258408ba0b02c05c
Julia Hromada <hromada.julia@gmail.com>
2024-09-11 15:01:11 +0200

    user Stories 3 done

--- components/ArtPieces/ArtPieces.js
+++ components/ArtPieces/ArtPieces.js
@@ -3,14 +3,14 @@ export default function ArtPieces({ pieces }) {
   return (
     <div>
       {pieces?.map((piece) => (
-        <li key={piece.slug}>
+        <div key={piece.slug}>
           <ArtPiecePreview
             title={piece.name}
             image={piece.imageSource}
             artist={piece.artist}
             slug={piece.slug}
           />
-        </li>
+        </div>
       ))}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,15 @@
-import useSWR from "swr";
-import ArtPieces from "@/components/ArtPiece/ArtPieces";
 import Spotlight from "@/components/Spotlight/Spotlight";
 
-const URL = "https://example-apis.vercel.app/api/art";
-
-function getRandomArtPiece(pieces) {
-  const randomIndex = Math.floor(Math.random() * pieces.length);
-  return pieces[randomIndex];
-}
-export default function HomePage() {
-  const fetcher = async (url) => {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-      const error = new Error("An error occurred while fetching the images.");
-      error.info = await res.json();
-
-      error.status = res.status;
-      throw error;
-    }
-    return res.json();
-  };
-
-  const { data, error } = useSWR(URL, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (!data) {
-    return <div>loading...</div>;
+export default function SpotlightPage({ pieces }) {
+  function getRandomArtPiece(pieces) {
+    const randomIndex = Math.floor(Math.random() * pieces.length);
+    return pieces[randomIndex];
   }
-
-  const randomPiece = getRandomArtPiece(data);
+  const randomPiece = getRandomArtPiece(pieces);
 
   return (
     <div>
       <h1>Art Gallery </h1>
-      <ArtPieces pieces={data} />
       <Spotlight image={randomPiece.imageSource} artist={randomPiece.artist} />
     </div>
   );
